428e99b3e8a7592d0e28e9fab6e25b226ac1bc21
thommccarthy <mccarttj@gmail.com>
2021-02-19 13:58:39 -0500

    first commit

---
+++ src/components/layout/Navbar.js
@@ -0,0 +1,29 @@
+import React, { Component } from 'react'
+import PropTypes from 'prop-types';
+
+//throws error if prop type not a string
+export class Navbar extends Component {
+    static defaultProps = {
+        title: 'Github Finder',
+        icon: 'fab fa-github'
+    }
+
+//static is a method which can be created inside of a class
+static propTypes = {
+    title: PropTypes.string.isRequired,
+    icon: PropTypes.string.isRequired
+}
+
+    render() {
+        return (
+            <nav className='navbar bg-primary'>
+                <h1>
+                    <i className={ this.props.icon } />
+                    { this.props.title }
+                </h1>
+            </nav>
+        )
+    }
+}
+
+export default Navbar
