7d32b2313ccd17d498435bf4494f16f0f9b3a920
thommccarthy <mccarttj@gmail.com>
2021-02-19 18:15:09 -0500

    refactoring, functionality same

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -1,30 +1,29 @@
-import React, { Component } from 'react'
+import React from 'react'
 import PropTypes from 'prop-types';
 
-//throws error if prop type not a string
-export class Navbar extends Component {
-    static defaultProps = {
-        title: '  Github Finder',
-        icon: 'fab fa-github'
-    }
 
-//static is a method which can be created inside of a class
-static propTypes = {
-    title: PropTypes.string.isRequired,
-    icon: PropTypes.string.isRequired
-}
+const Navbar = ({icon, title}) => {  
 
-    render() {
         return (
             <nav className='navbar bg-primary'>
                 <h1>
-                    {/* refers to default props */}
-                    <i className={ this.props.icon } />  
-                    { this.props.title }
+                    <i className={ icon } />  
+                    { title }
                 </h1>
             </nav>
         )
+    
     }
+
+Navbar.defaultProps = {
+    title: '  Github Finder',
+    icon: 'fab fa-github'
+}
+
+
+Navbar.propTypes = {
+title: PropTypes.string.isRequired,
+icon: PropTypes.string.isRequired
 }
 
 export default Navbar
