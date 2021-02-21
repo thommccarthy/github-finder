e27f366c34573b8042d4651d488f439081e55faf
thommccarthy <mccarttj@gmail.com>
2021-02-21 13:21:53 -0500

    added state and event listener to search box

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ class App extends Component {
 
     //this lifecycle method runs when project is mounted
     async componentDidMount() {
-      console.log(process.env.REACT_APP_GITHUB_CLIENT_SECRET)
+      // console.log(process.env.REACT_APP_GITHUB_CLIENT_SECRET)
       this.setState({ loading: true });
 
       const res = await axios.get(`https://api.github.com/users?client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`);
--- src/Users/Search.js
+++ src/Users/Search.js
@@ -1,12 +1,29 @@
 import React, { Component } from 'react'
 
+
 //forms in react generally have state on input
 class Search extends Component {
+
+        state = {
+        text: '' 
+        };
+
+//updates the state of text with onChange method
+onChange = (e) => {
+    this.setState({ text: e.target.value });
+};
+
+//use arrow functions inside react objects
+//"this" keyword responds differently
+onSubmit = (e) => {
+ console.log(this.state.text);
+}
+
     render() {
         return (
             <div>
-               <form className='form'>
-                <input type='text' name='text' placeholder='Search Users...'/>
+               <form onSubmit={this.onSubmit} className="form">
+                <input type="text" name="text" placeholder="Search Users..." value={this.state.text} onChange={this.onChange}/>
                 <input type="submit" value="Search" className="btn btn-dark btn-block"/>
                </form> 
             </div>
