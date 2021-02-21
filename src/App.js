0a4e286e77f4f020b17574e7827254792d558b26
thommccarthy <mccarttj@gmail.com>
2021-02-20 21:46:30 -0500

    added client id and secret to get request

--- src/App.js
+++ src/App.js
@@ -12,9 +12,10 @@ class App extends Component {
 
     //this lifecycle method runs when project is mounted
     async componentDidMount() {
+      console.log(process.env.REACT_APP_GITHUB_CLIENT_SECRET)
       this.setState({ loading: true });
 
-      const res = await axios.get('https://api.github.com/users');
+      const res = await axios.get(`https://api.github.com/users?client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`);
 
       this.setState({users: res.data, loading: false});
     }
